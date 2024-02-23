ecf947154cfb8b5935ed4ae07ff52688e9804980
johnray000 <johnray000@gmail.com>
2024-02-23 19:49:15 -0300

    agregado grafico de prueba default con tolltip

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import bexoLogo from "./assets/bexo-logo.png";
-import CardPrueba from "./components/CardPrueba";
+import { Grafico_ARS_withCustomTooltip } from "./components/GraficoARSwithCustomTooltip";
 
 //import "./App.css";
 
@@ -19,20 +19,17 @@ function App() {
           alt="Vite logo"
         />{" "}
       </div>
-      <h4 className="md:col-span-2 border border-yellow-400">Menu</h4>
-      <div className="card row-span-4 md:row-span-9 border  border-yellow-400">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h4 className="md:col-span-2">Menu</h4>
+      <div className="grid place-items-center row-span-4 md:row-span-9 border rounded-lg border-gray-300">        
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          PROBRAR GRAFICO VES CUSTOM DE UNA
         </p>
       </div>
       <p className="md:col-start-1">
         Click on the Vite and React logos to learn more
       </p>
-      <div className="md:col-start-2 md:row-start-3 md:row-span-9 row-span-4 border border-yellow-400">
-        <CardPrueba />
+      <div className="md:col-start-2 md:row-start-3 md:row-span-9 row-span-4 border rounded-lg border-gray-300">
+        <Grafico_ARS_withCustomTooltip />
       </div>
       <p className="">Precios 2</p>
     </div>
