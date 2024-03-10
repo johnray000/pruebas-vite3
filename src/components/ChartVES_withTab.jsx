829826d1bd53d31acdbfc8e9c07792f628e1e2e9
johnray000 <johnray000@gmail.com>
2024-03-10 20:21:31 -0300

    haciendo commit en main cmbiado la ruta de la bandera vzla-flag cargando desde public

--- src/components/ChartVES_withTab.jsx
+++ src/components/ChartVES_withTab.jsx
@@ -55,7 +55,7 @@ export default () => {
           <Title className="text-base px-2">
             Historico Dolar
           </Title>
-          <img src="../src/assets/vzla-flag.png" className="h-8 -mt-1" />
+          <img src="../public/vzla-flag.png" className="h-8 -mt-1" />
         </div>
         <TabGroup
           index={selectedIndex}
