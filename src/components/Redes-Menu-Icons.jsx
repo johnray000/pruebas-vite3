72adb141c194f4fca253e4fd8c6eb36278286f00
johnray000 <johnray000@gmail.com>
2024-06-28 17:53:27 -0300

    agregados todos los links redes default

--- src/components/Redes-Menu-Icons.jsx
+++ src/components/Redes-Menu-Icons.jsx
@@ -5,14 +5,16 @@ export default () => {
   return (
     <div className="mx-auto max-w-52 flex items-center">
       <a href="https://wa.me/541123913064" target="_blank" className="mt-1 mx-auto">
-        <RiWhatsappLine size={30} className=" text-green-500" />
+        <RiWhatsappLine size={30} className=" text-green-600" />
       </a>
       <a href="https://www.instagram.com/bexo.com.ve" target="_blank" className="mt-1 mx-auto">
-      <RiInstagramLine size={30} className="mt-1 mx-auto text-indigo-800" />
+      <RiInstagramLine size={30} className="mt-1 mx-auto text-pink-500" />
       </a>
-      <RiFacebookCircleLine size={30} className="mt-1 mx-auto text-gray-500" />
-      <a href="https://blog.bexo.com.ve" target="_self" className="mt-1 mx-auto">
-      <RiArticleLine size={30} className=" text-pink-500" />
+      <a href="https://www.facebook.com/100092000640446" target="_blank" className="mt-1 mx-auto">
+      <RiFacebookCircleLine size={30} className="mt-1 mx-auto text-blue-900" />
+      </a>
+      <a href="https://blog.bexo.com.ve" target="_blank" className="mt-1 mx-auto">
+      <RiArticleLine size={30} className=" text-orange-500" />
       </a>
     </div>
     
